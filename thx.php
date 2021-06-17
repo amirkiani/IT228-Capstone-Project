a82f79fe620900b9226f1c1c6e0b159a11994dca
Amir Kiani <53153060+amirkiani@users.noreply.github.com>
2021-06-17 13:44:35 -0700

    Create thx.php

---
+++ thx.php
@@ -0,0 +1,22 @@
+<?php
+include('config.php'); 
+include('includes/header.php');
+?>
+
+
+<div id="wrapper">
+
+<main>
+
+    <h1><?php echo $mainHeadLine;  ?></h1>
+    
+</main>
+
+<aside>
+<h3>Thank you for your feedback!</h3>
+</aside>
+
+  
+<?php
+include('includes/footer.php');
+?>
